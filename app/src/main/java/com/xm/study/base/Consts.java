477dce787b4fd01165ed0c4f5b787bd769486e2e
xm <1575587348@qq.com>
2017-08-31 14:39:51 +0800

    添加Consts文件

---
+++ app/src/main/java/com/xm/study/base/Consts.java
@@ -0,0 +1,14 @@
+package com.xm.study.base;
+
+/**
+ * @author: xm on 2017/8/31.
+ * @describe:
+ */
+
+public class Consts {
+
+    public static final String PATH_APP = "study/";
+    public static final String PATH_PATCH = "patch/";
+    public static final String PATH_PATCH_NUWA="patch_nuwa.jar";
+
+}
