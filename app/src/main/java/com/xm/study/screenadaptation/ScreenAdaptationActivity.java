bdc463d6a02ec80ebdc663141cf582f339e437d9
xm <1575587348@qq.com>
2017-09-19 17:01:30 +0800

    添加屏幕适配

---
+++ app/src/main/java/com/xm/study/screenadaptation/ScreenAdaptationActivity.java
@@ -0,0 +1,15 @@
+package com.xm.study.screenadaptation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xm.study.R;
+
+public class ScreenAdaptationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_adaptation);
+    }
+}
