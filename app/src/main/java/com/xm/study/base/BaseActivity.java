b125c68f6c57357b7870828d5d938a8bb3bd61bf
zhangxiumei <zhangxiumei@moxiu.net>
2019-08-08 12:36:51 +0800

    a1--BaseActivity--7

--- app/src/main/java/com/xm/study/base/BaseActivity.java
+++ app/src/main/java/com/xm/study/base/BaseActivity.java
@@ -40,6 +40,7 @@ public class BaseActivity extends AppCompatActivity implements IView, View.OnCli
         LogUtils.e(TAG,"a1---5");
         LogUtils.e(TAG,"a1---6");
         LogUtils.e(TAG,"a2---1");
+        LogUtils.e(TAG,"a1---7");
     }
 
 
